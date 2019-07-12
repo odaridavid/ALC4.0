6e40cb7e237b0d05a1be7a305a445c365f044538
Davidodari <davidkibzodari@gmail.com>
2019-07-13 02:19:22 +0300

    Create Web view for alc

--- app/src/main/java/com/lca/odari/alc40/ui/AlcFragment.kt
+++ app/src/main/java/com/lca/odari/alc40/ui/AlcFragment.kt
@@ -1,12 +1,18 @@
 package com.lca.odari.alc40.ui
 
 
+import android.net.http.SslError
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.SslErrorHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.lca.odari.alc40.R
+import kotlinx.android.synthetic.main.fragment_alc_view.view.*
 
 
 class AlcViewFragment : Fragment() {
@@ -15,7 +21,24 @@ class AlcViewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_alc_view, container, false)
+        val view = inflater.inflate(R.layout.fragment_alc_view, container, false)
+        view.webview.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                val message = when (error?.primaryError) {
+                    SslError.SSL_DATE_INVALID -> "Date Invalid"
+                    SslError.SSL_EXPIRED -> "Expired"
+                    SslError.SSL_IDMISMATCH -> "ID Mismatch"
+                    SslError.SSL_INVALID -> "Invalid"
+                    SslError.SSL_NOTYETVALID -> "Not Yet Valid"
+                    SslError.SSL_UNTRUSTED -> "Untrusted"
+                    else -> "Unknown"
+                }
+                Toast.makeText(context, "SSL Certificates : $message", Toast.LENGTH_LONG).show()
+                handler?.proceed()
+            }
+        }
+        view.webview.loadUrl("http://andela.com/alc/")
+        return view
     }
 
 
