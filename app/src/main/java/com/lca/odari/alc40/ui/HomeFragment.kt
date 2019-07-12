8e63ce263cef37cd9d7234ea5fb9f2242f052fe4
Davidodari <davidkibzodari@gmail.com>
2019-07-12 23:28:08 +0300

    Home Screen Implementation
    - add fonts
    - label fragments

--- app/src/main/java/com/lca/odari/alc40/ui/HomeFragment.kt
+++ app/src/main/java/com/lca/odari/alc40/ui/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.lca.odari.alc40.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
@@ -15,7 +17,14 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.about_alc_button.setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_homeFragment_to_alcViewFragment)
+        }
+        view.my_profile_button.setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_homeFragment_to_alcParticipantFragment)
+        }
+        return view
     }
 
 
