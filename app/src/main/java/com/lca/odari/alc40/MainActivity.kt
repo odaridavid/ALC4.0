f9cfaff38907d7451dfc327bfe7853bb1d38acfa
Davidodari <davidkibzodari@gmail.com>
2019-07-12 23:17:59 +0300

    Back button support

--- app/src/main/java/com/lca/odari/alc40/MainActivity.kt
+++ app/src/main/java/com/lca/odari/alc40/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.lca.odari.alc40
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }
